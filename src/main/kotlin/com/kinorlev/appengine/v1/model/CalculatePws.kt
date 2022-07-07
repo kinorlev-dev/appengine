3561449914085be7a187a949a3c7adba65836740
andriel <adielbot@gmail.com>
2022-07-07 10:33:32 +0300

    set up api doc with spring fox and swagger

--- src/main/kotlin/com/kinorlev/appengine/v1/model/CalculatePws.kt
+++ src/main/kotlin/com/kinorlev/appengine/v1/model/CalculatePws.kt
@@ -1,7 +1,15 @@
 package com.kinorlev.appengine.v1.model
 
+import io.swagger.annotations.ApiModel
+import io.swagger.annotations.ApiModelProperty
+
 class CalculatePwsBody(val data: List<PwsData> = emptyList())
-data class PwsData(val ts: Long = 0, val wave: Long = 0)
+@ApiModel("Sensor raw data")
+data class PwsData(
+    @ApiModelProperty("Ts in ms of initialization of Entity (not from sensor)")
+    val ts: Long = 0,
+    @ApiModelProperty("Received from sensor (not calculated)")
+    val wave: Long = 0)
 
 class CalculatePwsResponse(val message: String = "") : BaseResponse("CalculatePws")
 
